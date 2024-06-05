884acd84a8c88cb18e61535e6975bcc882ba482e
Troudi27 <troudi27troudi@gmail.com>
2024-06-05 02:13:44 +0100

    Completed the Rightbar Comp UI Design ...

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,6 +2,32 @@ import "./Rightbar.css"
 
 export default function Rightbar() {
   return (
-    <div className="rightbar">right</div>
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="/assets/gift.png" alt="" />
+          <span className="birthdayText"><b>Houssem Troudi </b> and <b> 3 others friends </b> have a birthday today</span>
+        </div>
+        <img className="rightbarAd" src="/assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friens</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className="rightbarProfileImg" src="/assets/person/10.jpeg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsernam">Houssem Troudi</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className="rightbarProfileImg" src="/assets/person/5.jpeg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsernam">Houssem Troudi</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
