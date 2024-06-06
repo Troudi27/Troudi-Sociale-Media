14fc9589747d8a68cd755875b3cea78ecf9c8280
Troudi27 <troudi27troudi@gmail.com>
2024-06-06 01:32:08 +0100

    Like Fonctionality with useState Hook

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,10 +1,15 @@
 import { MoreVert } from "@mui/icons-material"
 import "./Post.css"
 import {Users} from "../../dummyData"
+import { useState } from "react"
 
 export default function Post({post}) {
-    const user = Users.filter(u=>u.id===1)
-    console.log(user[0].username);
+   const [like,setLike] = useState(post.like)
+   const [isLiked,setIsLiked] = useState(false)
+   const likeHandler = ()=>{
+    setLike(isLiked ? like -1 : like + 1)
+    setIsLiked(!isLiked)
+   }
   return (
     <div className="post">
         <div className="postWrapper">
@@ -24,9 +29,9 @@ export default function Post({post}) {
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
-                    <img className="postLikeIcon"src="/assets/like.png" alt="" />
-                    <img className="postLikeIcon"src="/assets/heart.png" alt="" />
-                    <span className="postLikeCounter">{post.like} people like it</span>
+                    <img className="postLikeIcon"src="/assets/like.png" alt="" onClick={likeHandler} />
+                    <img className="postLikeIcon"src="/assets/heart.png" alt="" onClick={likeHandler} />
+                    <span className="postLikeCounter">{like} people like it</span>
                 </div>
                 <div className="postBottomRight">
                     <span className="postCommentText">{post.comment} comments</span>
