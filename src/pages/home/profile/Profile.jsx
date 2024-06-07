36a9574e1b45eda43601bbe65874ca5398c6a7dc
Troudi27 <troudi27troudi@gmail.com>
2024-06-07 16:05:59 +0100

    design photo provile and cover

---
+++ src/pages/home/profile/Profile.jsx
@@ -0,0 +1,37 @@
+import Rightbar from "../../../components/rightbar/Rightbar"
+import Sidebar from "../../../components/sidebar/Sidebar"
+import Topbar from "../../../components/topbar/Topbar"
+import Feeed from "../../../components/feed/Feed";
+
+import "./Profile.css"
+
+export default function Profile() {
+  return (
+    <div>
+      <Topbar/>
+      <div className="profile">
+      <Sidebar/>
+      <div className="profileRight"> 
+      <div className="profileRightTop">
+        <div className="profileCover">
+            <img className="profileCoverImg" src="assets/post/3.jpeg" alt="" />
+            <img className="profileUserImg" src="assets/person/3.jpeg" alt="" />
+        </div>
+        <div className="profileInfo">
+            <h4 className="profileInfoName">Houssem Troudi</h4>
+            <span className="profileInfoDesc">kklkl;kl;kl</span>
+        </div>
+      </div>
+        <div className="profileRightBottom">
+      <Feeed/>
+      <Rightbar/>
+     
+      </div>
+      </div>
+      
+
+      </div>
+      
+    </div>
+  )
+}
