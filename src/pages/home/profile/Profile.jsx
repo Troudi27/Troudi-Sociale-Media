437b0e08a86aa1be4793757c3a9df205a7f9f7f4
Troudi27 <troudi27troudi@gmail.com>
2024-06-07 19:28:08 +0100

    completed UI design profile page

--- src/pages/home/profile/Profile.jsx
+++ src/pages/home/profile/Profile.jsx
@@ -24,7 +24,7 @@ export default function Profile() {
       </div>
         <div className="profileRightBottom">
       <Feeed/>
-      <Rightbar/>
+      <Rightbar profile/>
      
       </div>
       </div>
